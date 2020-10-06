4f4a011d37a215375b4d9c59d9e276d2d9dc6ad9
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-10-06 11:19:47 -0600

    Unit  8 part 1

--- hr-timesheet/src/app/app.module.ts
+++ hr-timesheet/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { MaterialModule } from './modules/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './modules/app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
 import { DepartmentsComponent } from './components/departments/departments.component';
 import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -17,6 +17,7 @@ export class TimesheetComponent implements OnInit {
   employees: Employee[] = [];
   employeeId = 0;
   departmentRouteParam = 'id';
+  weekdays: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   constructor(
     private route: ActivatedRoute,
@@ -37,6 +38,13 @@ export class TimesheetComponent implements OnInit {
         departmentId: this.department.id,
         name: this.employeeNameFC.value,
         payRate: Math.floor(Math.random() * 50) + 50,
+        monday: 0,
+        tuesday: 0,
+        wednesday: 0,
+        thursday: 0,
+        friday: 0,
+        saturday: 0,
+        sunday: 0,
       });
 
       this.employeeNameFC.setValue('');
