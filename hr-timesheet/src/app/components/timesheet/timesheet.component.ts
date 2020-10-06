0a3b9764ed7a8c3ad102a766731b18071b58c969
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-10-06 13:12:27 -0600

    Unit 13 part 2 complete

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
 import { FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Employee } from '../../interfaces/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-timesheet',
@@ -21,7 +22,9 @@ export class TimesheetComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private departmentsService: DepartmentsService
+    private departmentsService: DepartmentsService,
+    private employeeService: EmployeeService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -34,7 +37,7 @@ export class TimesheetComponent implements OnInit {
       this.employeeId++;
 
       this.employees.push({
-        id: this.employeeId.toString(),
+        // id: this.employeeId.toString(),
         departmentId: this.department.id,
         name: this.employeeNameFC.value,
         payRate: Math.floor(Math.random() * 50) + 50,
@@ -72,6 +75,13 @@ export class TimesheetComponent implements OnInit {
 
   deleteEmployee(index: number): void {
     this.employees.splice(index, 1);
-}
+  }
+
+  submit(): void {
+    this.employees.forEach(employee => {
+      this.employeeService.saveEmployeeHours(employee);
+    });
+    this.router.navigate(['./departments']);
+  }
 
 }
